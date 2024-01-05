a5b3bff0d9226b169737dd2646f9a18b0cc42b09
kush.varade.19 <kush.varade.19@gmail.com>
2024-01-05 21:50:39 +0530

    Setup router service using gin-gonic for http communication

---
+++ config/router.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRouter is responsible for enabling HTTP requests using Gin for this application.
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	// TODO: Uncomment the code when the application supports this mode.
+	// If you wish to temporarily run this in release mode, you can use the command below.
+	// RUN: export GIN_MODE=release
+
+	// gin.SetMode(gin.ReleaseMode)
+
+	return router
+}
+
+// StartServer will initiate the server on the specified port.
+// If the provided port is empty or if the router is nil, the application will be closed.
+func StartServer(r *gin.Engine, port string) {
+	if port == "" {
+		log.Fatalln("Unable to start the server because of missing port.")
+	}
+
+	if r == nil {
+		log.Fatalln("Unable to start the server because of missing router.")
+	}
+
+	err := r.Run(fmt.Sprintf(":%s", port))
+
+	if err != nil {
+		log.Fatalf("Unable to start the application on port %s. Encountered an error %s while attempting to start the application.", port, err.Error())
+	}
+}
--- main.go
+++ main.go
@@ -4,17 +4,31 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/varadekd/card-game/config"
 	"github.com/varadekd/card-game/helper"
 )
 
+var router *gin.Engine
+
+// TODO: Hardcoding values in the code is incorrect; it should retrieve values from the environment file.
+const APP_PORT = "8080"
+
+// The init function is responsible for enabling all the essential services
+// required for this application's functionality.
 func init() {
+	fmt.Println("Setting up the initial services required by the server.")
+
 	err := helper.GenerateDefaultDeck()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	router = config.SetupRouter()
 }
 
 func main() {
-	fmt.Println("Starting application.")
+	fmt.Printf("Starting application on port %s\n", APP_PORT)
+	config.StartServer(router, APP_PORT)
 }
